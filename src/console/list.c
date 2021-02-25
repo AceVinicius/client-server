f7c35c653df6549fe9e8bb768f7dc9f3875704b6
acevinicius <acevinicius@icloud.com>
2021-02-25 16:38:36 -0300

    Swaped the "strange" triple pointer solution with a dynamic linked list, improving readability and maintainability. Also refactored execute.c with a much cleaner solution.

---
+++ src/console/list.c
@@ -0,0 +1,8 @@
+//
+//  list.c
+//  
+//
+//  Created by Vinícius Aguiar on 25/02/21.
+//
+
+#include "list.h"
---
+++ src/console/list.h
@@ -0,0 +1,13 @@
+//
+//  list.h
+//  
+//
+//  Created by Vinícius Aguiar on 25/02/21.
+//
+
+#ifndef list_h
+#define list_h
+
+#include <stdio.h>
+
+#endif /* list_h */
