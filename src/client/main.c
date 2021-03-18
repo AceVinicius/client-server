07af4b838296accd82277592c6d897b8e5bd48b2
acevinicius <acevinicius@icloud.com>
2021-03-18 13:37:50 -0300

    Renamed console folder to client

--- src/client/main.c
+++ src/client/main.c
@@ -25,9 +25,14 @@
 #include "../../lib/include/parse.h"
 #include "../../lib/include/client.h"
 #include "../../lib/include/general.h"
+#include "../../lib/include/sockets.h"
 #include "../../lib/include/history.h"
 #include "../../lib/include/execute.h"
 #include "../../lib/include/allocation.h"
+
+
+
+
 
 
 
